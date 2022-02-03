23f9991a47ca315d32585946225495ce87ad542e
kells <pashaglytov2004@mail.ru>
2022-02-03 19:46:17 +0300

    completed

---
+++ password/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <fstream>
+
+std::ofstream fout("output.txt");
+
+class Generator {
+private:
+    int lenght;
+    char *password;
+public:
+    Generator() {
+        this->lenght = 9;
+        password = new char[9]{'P','A','S','S','W','O','R','D','\0'};
+
+    }
+    ~Generator() {
+        delete password;
+    }
+
+    void getLenght(const int &lenght) {
+        this->lenght = lenght;
+    }
+
+    void print() {
+        std::cout << "lenght : " << lenght << "\n";
+        std::cout << "your password : " << password << "\n";
+        fout << "lenght : " << lenght << "\n";;
+        fout << "your password : " << password << "\n";
+    }
+
+    int chooseLevel() {
+        std::cout << "Enter hard level of your password\n";
+        
+        int hardLevel;
+                                    
+        std::cout << "Choose :\n 1. easy\n 2. hard\n";
+        std::cin >> hardLevel;
+
+        return hardLevel;
+    }
+
+    char* easyGenerator(int begin, int end) {
+        password = new char[lenght + 1];
+        for (int i = 0; i < lenght; i++) {
+            password[i] = rand() % begin + end;
+        }
+        password[lenght] = '\0';
+        return password;
+    }
+
+    char* hardGenerator(int begin1,int end1, int begin2, int end2, int begin3, int end3) {
+        password = new char[lenght + 1];
+
+        for (int i = 0; i < lenght; i += 3) {
+            password[i] = rand() % begin1 + end1;
+        }
+        for (int i = 1; i < lenght; i += 3) {
+            password[i] = rand() % begin2 + end2;
+        }
+        for (int i = 2; i < lenght; i += 3) {
+            password[i] = rand() % begin3 + end3;
+        }
+        password[lenght] = '\0';
+
+        return password;
+    }
+
+    char* passwordGenerator(const int &lenght) {
+        int hardLevel = chooseLevel();
+        int chance = rand() % 6 + 1;
+
+        switch (hardLevel) {
+
+        case 1:
+            int easyLevel;
+            std::cout << "Choose your password consists of\n";
+            std::cout << "1. numbers\n2. UPPERCASE_letter\n3. lowercase_letters\n";
+            std::cin >> easyLevel;
+            switch (easyLevel) {
+            case 1:
+                password = easyGenerator(10, 48);
+                break;
+            case 2:
+                password = easyGenerator(26, 65);
+                break;
+            case 3:
+                password = easyGenerator(26, 97);
+                break;
+            default:
+                password = easyGenerator(26, 65);
+            }
+            break;
+
+        case 2:
+
+            switch (chance) {
+            case 1:
+                password = hardGenerator(26, 97, 10, 48, 26, 65);
+                break;
+            case 2:
+                password = hardGenerator(26, 97, 26, 65, 10, 48);
+                break;
+            case 3:
+                password = hardGenerator(10, 48, 26, 97, 26, 65);
+                break;
+            case 4:
+                password = hardGenerator(10, 48, 26, 65, 26, 97);
+                break;
+            case 5:
+                password = hardGenerator(26, 65, 26, 97, 10, 48);
+                break;
+            case 6:
+                password = hardGenerator(26, 65, 10, 48, 26, 97);
+                break;
+            }
+
+            break;
+
+        default:
+
+            password = easyGenerator(26, 97);
+
+        }
+
+        return password;
+
+    }
+};
+
+class ASCII {
+public:
+    void getTable() {
+        for (int i = 0; i < 255; i++) {
+            std::cout << i << " " << char(i) << "\n";
+        }
+    }
+};
+
+int main()
+{
+    srand(time(NULL));
+
+    Generator A;
+
+    std::cout << "Enter the lenght of your password (min 8 symbols) :\t";
+    int lenght;
+    std::cin >> lenght;
+    if (lenght < 8) {
+        std::cout << "Wrong! Automatically it was turned to 8\n";
+        lenght = 8;
+    }
+
+    A.getLenght(lenght);
+    A.passwordGenerator(lenght);
+    A.print();
+
+    return 0;
+}
---
+++ words/words/words.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+std::ifstream fin("input.txt");
+
+class Word {
+private:
+    std::string* arr;
+    int attempts = 6;
+    int chance;
+    int closedLetters = 0;
+public:
+
+    void getData() {
+        int size;
+        fin >> size;
+        chance = rand() % size + 1;
+        arr = new std::string[size];
+        for (int i = 0; i < size; i++) {
+            std::getline(fin, arr[i]);
+        }
+    }
+
+    std::string transform() {
+        std::string s = arr[chance - 1];
+        for (int i = 0; i < arr[chance - 1].size()-2; i++) {
+            s[i] = '*';
+        }
+        return s;
+    }
+    
+    void statusFunction() {
+        std::cout << "Word - ";
+    }
+
+    void guessFunction(std::string& cipherWord) {
+        std::cout << "Guess the letter : ";
+        char symbol;
+        std::cin >> symbol;
+        bool status = false;
+        for (int i = 0; i < arr[chance - 1].size() - 2; i++) {
+            if (symbol == arr[chance - 1][i]) {
+                status = true;
+                arr[chance - 1][i] = '*';
+                cipherWord[i] = symbol;
+                std::cout << "Your word : " << cipherWord << "\n";
+                std::cout << "Your attemps : " << attempts << "\n";
+                ++closedLetters;
+                if (closedLetters == arr[chance - 1].size() - 2) {
+                    std::cout << "CONGRATILATIONS, YOU WIN!\n";
+                    break;
+                }
+                else {
+                    guessFunction(cipherWord);
+                }
+            }
+        }
+
+        if(!status) {
+        --attempts;
+        std::cout << "There is no such letter\n";
+        std::cout << "Your word : " << cipherWord << "\n";
+        std::cout << "Your attemps :" << attempts << "\n";
+            if (attempts > 0) {
+                guessFunction(cipherWord);
+            }
+            else {
+                std::cout << "YOU LOST!\n";
+            }
+        }
+
+    }
+};
+
+int main()
+{
+    srand(time(NULL));
+
+    if (!fin.is_open()) {
+        std::cout << "Error\n";
+    }
+    else {
+        std::cout << "File was opened\n";
+    }
+ 
+    Word A;
+    A.getData();
+    std::string cipherWord = A.transform();
+
+    std::cout << "Computer thought of a word : " << cipherWord << "\n";
+    A.guessFunction(cipherWord);
+    return 0;
+}
